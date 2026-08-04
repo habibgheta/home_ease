1a3e51d1d836eeee186474f100a77c52e6d6a295
Habib Gheta <ghetahabib@gmail.com>
2026-08-04 16:24:46 +0530

    Integrate Firebase with Android and Web

--- android/app/src/main/kotlin/com/homeease/platform/MainActivity.kt
+++ android/app/src/main/kotlin/com/homeease/platform/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.home_ease
+package com.homeease.platform
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:home_ease/screens/auth/login_screen.dart';
+import 'package:home_ease/firebase_options.dart';
 import 'package:home_ease/screens/main_screen.dart';
 import 'theme/app_theme.dart';
-import 'screens/splash/splash_screen.dart';
-import 'package:home_ease/screens/home/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
+
   runApp(const HomeEaseApp());
 }
 
